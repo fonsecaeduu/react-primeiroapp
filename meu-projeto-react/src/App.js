55d0d45df16568df3a2424c0b600696f411d26a6
Eduardo Fonseca <153040743+fonsecaeduu@users.noreply.github.com>
2024-08-13 02:38:22 +0000

    Completo

---
+++ meu-projeto-react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import MeuComponente from './MeuComponente';
+
+function App() {
+  return (
+    <div className="App">
+      <MeuComponente />
+      <hr />
+      <MeuComponente />
+      <hr />
+      <MeuComponente />
+    </div>
+  );
+}
+
+export default App;
---
+++ meu-projeto-react/src/MeuComponente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MeuComponente() {
+    return (
+        <div>
+            <img src="https://petitgato.com.br/img/webp/gatos-persas-em-sao-paulo-img-3780.webp"/>
+            <p>Texto de Teste Testando</p>
+        </div>
+    );
+}
+
+export default MeuComponente;
